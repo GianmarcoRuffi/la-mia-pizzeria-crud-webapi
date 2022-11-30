a01f7f7266e54a430725ed57b4c96ae513a027fb
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-11-30 13:40:07 +0100

    Aggiunto Model Message

---
+++ Models/Message.cs
@@ -0,0 +1,53 @@
+﻿using la_mia_pizzeria_static.ValidationCustomClasses;
+using Microsoft.Identity.Client;
+using System.ComponentModel.DataAnnotations;
+
+namespace la_mia_pizzeria_static.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo richiesto")]
+        [StringLength(300, ErrorMessage = "La lunghezza eccede i caratteri consentiti")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Campo richiesto")]
+        [StringLength(300, ErrorMessage = "La lunghezza eccede i caratteri consentiti")]    
+        [FiveWordsValidation]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Campo richiesto")]
+        [StringLength(300, ErrorMessage = "La lunghezza eccede i caratteri consentiti")]
+        public string? Title { get; set; }
+
+        [Required(ErrorMessage = "Campo richiesto")]
+        [StringLength(300, ErrorMessage = "La lunghezza eccede i caratteri consentiti")]
+        public string? Text { get; set; }
+
+
+        public Message()
+        {
+
+
+
+
+        }
+
+        public Message(string email, string name, string title, string text)
+        {
+
+            this.Email = email;
+            this.Name = name;
+            this.Title = title;
+            this.Text = text;
+        }
+    }
+
+
+
+
+
+
+
+}
