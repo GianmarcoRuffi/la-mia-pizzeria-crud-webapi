7f81de8c55868df547408e0998245b73f047061c
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-11-30 17:11:59 +0100

    Aggiunto js a view

--- Controllers/API/MessageController.cs
+++ Controllers/API/MessageController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using la_mia_pizzeria_static.Models;
+using la_mia_pizzeria_static.Data;
 
 namespace la_mia_pizzeria_static.Controllers.API
 {
@@ -8,10 +9,29 @@ namespace la_mia_pizzeria_static.Controllers.API
     [ApiController]
     public class MessageController : ControllerBase
     {
+        private PizzaDbContext db;
+
+        public MessageController(PizzaDbContext _db)
+        {
+            db = _db;
+        }
         [HttpPost]
 
-        public IActionResult Post([FromBody] Message message)
+        public IActionResult Create([FromBody] Message message)
+        {
 
-        { return Ok("message"); }
+            try
+            {
+                db.Messages.Add(message);
+                db.SaveChanges();
+
+            }
+            catch (Exception e)
+            {
+                return UnprocessableEntity(e.Message);
+            }
+
+            return Ok(message);
+        }
     }
 }
